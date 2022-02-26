f5e6f745df3cf6af936b6477b3a4b5d0b65cb55b
Cristian Popescu <38956238+cristipope23@users.noreply.github.com>
2022-02-26 19:28:30 +0200

    Add files via upload

---
+++ Password_Checker/checkmypass.py
@@ -0,0 +1,64 @@
+import requests
+import hashlib  # for SHA1 Hashing
+
+
+def request_api_data(query_char):
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(
+            f'Error fetching: {res.status_code}, check the API and try again!')
+
+    return res
+
+
+# loop through hashesh and check
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:  # loop through tuple
+        if h == hash_to_check:
+            return count
+
+    return 0
+
+
+def pwned_api_check(password):
+    sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
+    return get_password_leaks_count(response, tail)
+
+
+def main(password_list):
+    for password in password_list:
+        count = pwned_api_check(password)
+        if count:
+            print(
+                f'{password} was found {count} times... you should change your password!')
+        else:
+            print(f'{password} was NOT found. Good password!')
+
+    return 'done!'
+
+
+def read_passwords_from_file():
+    try:
+        f = open('passwords.txt', 'r')
+    except IOError:
+        raise IOError('Text file did not open correctly!')
+
+    password_list = []
+    lines = f.read().splitlines()
+
+    for line in lines:
+        password = line
+
+        password_list.append(password)
+
+    return password_list
+
+
+if __name__ == '__main__':
+
+    password_list = read_passwords_from_file()
+    main(password_list)
